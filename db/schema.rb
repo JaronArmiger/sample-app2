ebc8a02fd1b092df37a43ec528ffb9301623745c
JaronArmiger <jaron.armiger@gmail.com>
2020-04-10 10:55:08 -0500

    implement basic login

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_04_10_003042) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
